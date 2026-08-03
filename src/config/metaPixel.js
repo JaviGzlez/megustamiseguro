a1ba59c551273741eaa535e60099ab43233068ee
Javier Gonzalez Zarza <javier.gonzalez.zarza@gmail.com>
2026-08-03 14:07:44 +0200

    Meta Pixel 2.0

--- src/config/metaPixel.js
+++ src/config/metaPixel.js
@@ -3,4 +3,4 @@
 // Sustituye este valor por tu Pixel ID real de Meta Business Suite:
 // Meta Business Suite > Orígenes de datos > Píxeles > (tu píxel) > Detalles del píxel
 // El ID es una cadena numérica (ej. "1234567890123456").
-export const META_PIXEL_ID = "TU_PIXEL_ID_AQUI";
+export const META_PIXEL_ID = "917419218077959";
